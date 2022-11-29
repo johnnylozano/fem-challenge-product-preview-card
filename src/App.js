c1c149ef755fae4270d29316a12ac991ff242bdb
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-11-29 11:11:39 -0500

    feat: Initial html write up, add assets folder, cleared app.css default styling
    Wrote out html elements for text and image elements for the product card.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import './App.css';
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <main>
+            <article className="product__card">
+               <picture className="product__img">
+                  <img
+                     src={require('./assets/img/image-product-mobile.jpg')}
+                     alt="Gabrielle Essence perfume bottle laying on a table"
+                  />
+               </picture>
+
+               <div className="product__content">
+                  <p className="product__category">Perfume</p>
+
+                  <h1 className="product__title">
+                     Gabrielle Essence Eau De Parfum
+                  </h1>
+
+                  <p>
+                     A floral, solar and voluptuous interpretation composed by
+                     Olivier Polge, Perfumer-Creator for the House of CHANEL.{' '}
+                  </p>
+
+                  <div className="flex-group">
+                     <p className="product__sale-price">$149.99 </p>
+                     <p className="product__whole-price">$169.99 </p>
+                  </div>
+
+                  <button className="btn">Add to Cart</button>
+               </div>
+            </article>
+         </main>
+      </div>
+   );
 }
 
 export default App;
