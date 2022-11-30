b18062fd10696bf26f73abc9138a40215911d7a6
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-11-30 18:56:16 -0500

    fix: Removed linebreaks from product description text

--- src/components/ProductCard/ProductData.js
+++ src/components/ProductCard/ProductData.js
@@ -3,8 +3,8 @@ export const PerfumeData = {
    imgSm: require('../../assets/img/image-product-mobile.jpg'),
    productCategory: 'Perfume',
    productTitle: 'Gabrielle Essence Eau De Parfum',
-   description: `A floral, solar and voluptuous interpretation composed by Olivier
-    Polge, Perfumer-Creator for the House of CHANEL.`,
+   description:
+      'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL',
    salePrice: '$149.99',
    originalPrice: '$169.99',
 };
