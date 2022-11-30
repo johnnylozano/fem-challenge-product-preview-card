b9db8691d90e915feba790d6933ac5ab97a88135
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-11-30 18:52:31 -0500

    refactor: Turned html into components, styled components, and added product data

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
 import './App.css';
+import { ProductCard } from './components/ProductCard';
+import { PerfumeData } from './components/ProductCard/ProductData';
 
 function App() {
    return (
       <div className="App">
          <main>
-            <article className="product__card">
-               <picture className="product__img">
-                  <source
-                     srcSet={require('./assets/img/image-product-desktop.jpg')}
-                     media="(min-width: 600px)"
-                  />
-                  <img
-                     src={require('./assets/img/image-product-mobile.jpg')}
-                     alt="Gabrielle Essence perfume bottle laying on a table"
-                  />
-               </picture>
-
-               <div className="product__content">
-                  <p className="product__category">Perfume</p>
-
-                  <h1 className="product__title">
-                     Gabrielle Essence Eau De Parfum
-                  </h1>
-
-                  <p>
-                     A floral, solar and voluptuous interpretation composed by
-                     Olivier Polge, Perfumer-Creator for the House of CHANEL.{' '}
-                  </p>
-
-                  <div className="flex-group">
-                     <p className="product__sale-price">
-                        <span className="visually-hidden">On sale for:</span>
-                        $149.99
-                     </p>
-                     <p className="product__whole-price">
-                        <span className="visually-hidden">Original price:</span>
-                        <s>$169.99</s>
-                     </p>
-                  </div>
-
-                  <button className="btn" dataIcon="shopping-cart">
-                     Add to Cart
-                  </button>
-               </div>
-            </article>
+            <ProductCard {...PerfumeData} />
          </main>
       </div>
    );
---
+++ src/components/ProductCard/ProductData.js
@@ -0,0 +1,10 @@
+export const PerfumeData = {
+   imgLg: require('../../assets/img/image-product-desktop.jpg'),
+   imgSm: require('../../assets/img/image-product-mobile.jpg'),
+   productCategory: 'Perfume',
+   productTitle: 'Gabrielle Essence Eau De Parfum',
+   description: `A floral, solar and voluptuous interpretation composed by Olivier
+    Polge, Perfumer-Creator for the House of CHANEL.`,
+   salePrice: '$149.99',
+   originalPrice: '$169.99',
+};
