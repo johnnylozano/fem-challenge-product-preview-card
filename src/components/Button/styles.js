35ab483f5dd0c80e6375e258c7f7189f3ddf53c6
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-11-30 19:02:32 -0500

    refactor: Moved Button styles to styles file

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Btn = styled.button`
+   cursor: pointer;
+   text-decoration: none;
+   display: inline-flex;
+   justify-content: center;
+   align-items: center;
+   gap: 0.5rem;
+   padding: 0.75em 1.5em;
+   border: 0;
+   border-radius: 0.5rem;
+   background-color: var(--clr-primary-400);
+   color: var(--clr-neutral-100);
+   font-weight: var(--fw-bold);
+   font-size: 0.925rem;
+
+   &:is(:hover, :focus) {
+      background-color: var(--clr-primary-500);
+   }
+`;
